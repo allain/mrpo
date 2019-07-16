e0f5f93fa229cc99f301bea0e504e4b58fdc019d
Allain Lalonde <alalonde@elastalink.com>
2019-07-16 10:18:52 -0400

    supporting composite executors

--- examples/javascript-pkg/src/included.js
+++ examples/javascript-pkg/src/included.js
@@ -1,8 +1,7 @@
-import toLower from "lodash.tolower"
+// import toLower from "lodash.tolower"
 // import flatten from "lodash.flatten"
-import _ from "lodash"
+// import _ from "lodash"
 
 export default function greet() {
-  // console.log(toLower("HELLO"))
-  // console.log(flatten([[1], [2]]))
+  console.log("Hello")
 }
